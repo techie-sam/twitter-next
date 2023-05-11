7fe4e8aeaaa6029067c3623b81cf37c9c952bea5
TECHIE SAM <blessingabolade15@gmail.com>
2023-05-11 12:44:54 +0100

    added content of nav in notification route

--- components/Tweets.js
+++ components/Tweets.js
@@ -1,12 +1,12 @@
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 // import { PropTypes } from "prop-types";
 import { useRouter } from "next/router";
 
 
 const Tweets = () => {
     const route = useRouter()
-
     return (
         <div className="max-h-screen w-[90%] md:w-[50w] border border-gray-300  overflow-y-auto">
             <nav className="w-full border-b bg-white bg-opacity-50 backdrop-blur-lg border-gray-300 sticky top-0">
@@ -19,14 +19,24 @@ const Tweets = () => {
                 }
                 {
                     route.asPath === "/home" &&
-                    <div className="">
+                    <>
                         <h3 className="text-2xl font-bold p-5 hidden md:block">Home</h3>
                         <div className="text-center w-full flex">
                             <a href="#" className="p-5 w-full hover:bg-gray-200">For You</a>
                             <a href="#" className="p-5 w-full hover:bg-gray-200">Following</a>
                         </div>
-                    </div>
+                    </>
                 }
+                {
+                    route.asPath === "/notifications" &&
+                    <>
+                        <Link href="notifications">
+                            <h3 className="font-bold px-5 py-3 hidden md:block">Notifications</h3>
+                        </Link>
+                    </>
+                }
+
+
             </nav>
             <div className="overflow-x-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
 
