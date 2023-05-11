37175889092b5e7d142e901cb479acda489d9785
TECHIE SAM <blessingabolade15@gmail.com>
2023-05-11 15:16:16 +0100

    added  props to getStaticProps Function

--- pages/[route].js
+++ pages/[route].js
@@ -1,32 +1,14 @@
 import LeftSideNav from "@/components/LeftSideNav";
 import RightSideNav from "@/components/RightSideNav";
 import Tweets from "@/components/Tweets";
-import { faBell, faHamburger, faHashtag, faHouse, faMessage, faSave, faUser, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faHamburger, faHashtag, faHouse, faMessage, faSave, faUser } from "@fortawesome/free-solid-svg-icons";
 import Head from "next/head";
 
-const menuItems = [
-    { icon: faHouse, text: "Home", href: "home" },
-    { icon: faHashtag, text: "Explore", href: "explore" },
-    { icon: faBell, text: "Notifications", href: "notifications" },
-    { icon: faMessage, text: "Messages", href: "messages" },
-    { icon: faSave, text: "Bookmarks", href: "bookmarks" },
-    { icon: faUser, text: "Profile", href: "profile" },
-    { icon: faHamburger, text: "More", href: "more" },
-]
-
-const bottomNavContent = [
-    { text: faHouse, href: "home" },
-    { text: faBell, href: "notifications" },
-    { text: faMessage, href: "messages" },
-    { text: faUser, href: "messages" },
-]
-
-
-const PageType = () => {
+const PageType = ({ menuItems, bottomNavContent, title }) => {
     return (
         <>
             <Head>
-
+                <title>{title.toUpperCase()}</title>
             </Head>
             <div className='flex justify-center h-screen space-x-2'>
                 <LeftSideNav menuItems={menuItems} />
@@ -39,15 +21,36 @@ const PageType = () => {
 
 
 export async function getStaticProps({ params }) {
+
+    const menuItems = [
+        { icon: faHouse, text: "Home", href: "home" },
+        { icon: faHashtag, text: "Explore", href: "explore" },
+        { icon: faBell, text: "Notifications", href: "notifications" },
+        { icon: faMessage, text: "Messages", href: "messages" },
+        { icon: faSave, text: "Bookmarks", href: "bookmarks" },
+        { icon: faUser, text: "Profile", href: "profile" },
+        { icon: faHamburger, text: "More", href: "more" },
+    ]
+
+    const bottomNavContent = [
+        { text: faHouse, href: "home" },
+        { text: faBell, href: "notifications" },
+        { text: faMessage, href: "messages" },
+        { text: faUser, href: "messges" },
+    ]
+
     return {
         props: {
             title: params.route,
-            params
+            menuItems,
+            bottomNavContent
         }
     }
 }
 
+
 export async function getStaticPaths() {
+
     const paths = [
         { params: { route: "home" } },
         { params: { route: 'explore' } },
@@ -57,9 +60,11 @@ export async function getStaticPaths() {
         { params: { route: 'profile' } },
         { params: { route: 'more' } }
     ];
+
     return {
         paths,
         fallback: false
     };
 }
+
 export default PageType;
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,7 @@ import { faGear, faHashtag } from "@fortawesome/free-solid-svg-icons";
 // import { useRouter } from 'next/router';
 
 
-const menuItems = [
-  { icon: faHashtag, text: "Explore", href:"/" },
-]
 
-const bottomNavContent =[
-  { text:"Login", href:"#", icon:"" },
-  { text:"NavBar", href:"#", icon:"" }
-]
 
 // const rightBarContents = [
 //   { link: "#", text: "Sign in as" },
@@ -22,13 +15,32 @@ const bottomNavContent =[
 // ]
 
 
-export default function Home(props) {
+export default function Home({ menuItems, bottomNavContent }) {
   console.log(props);
   return (
     <main className='flex justify-center h-screen space-x-2'>
       <LeftSideNav menuItems={menuItems} />
-      <Tweets bottomNavContent={bottomNavContent}/>
+      <Tweets bottomNavContent={bottomNavContent} />
       <RightSideNav />
     </main>
   )
+}
+export function getStaticProps() {
+
+  const menuItems = [
+    { icon: faHashtag, text: "Explore", href: "/" },
+  ]
+
+  const bottomNavContent = [
+    { text: "Login", href: "login" },
+    { text: "NavBar", href: "navbar" }
+  ]
+
+  return {
+    props: {
+      menuItems,
+      bottomNavContent
+    },
+  }
+
 }
